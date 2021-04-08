af966362a8e5556e6f81efc5b90aed69fcf56f56
jonghyunpark85 <jonghyun.park0202@gmail.com>
2021-04-08 09:03:38 -0600

    Second

---
+++ vtf/src/components/layout/auth.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-app>
+    <v-main>
+      <v-container class="justify-center align-center fill-height">
+        <v-text-field outlined>fire</v-text-field>
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+
+@Component
+export default class LayoutAuth extends Vue {
+
+}
+</script>
+
+<style lang="scss">
+</style>
